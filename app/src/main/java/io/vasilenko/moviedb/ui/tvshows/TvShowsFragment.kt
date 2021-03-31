730e913d94c75f96d0609505db6fe6695a726a9f
Sergey Vasilenko <sergey@vasilenko.io>
2021-04-01 00:29:29 +0300

    Del unused fragment instantiation functions and arguments from tv shows

--- app/src/main/java/io/vasilenko/moviedb/ui/tvshows/TvShowsFragment.kt
+++ app/src/main/java/io/vasilenko/moviedb/ui/tvshows/TvShowsFragment.kt
@@ -7,21 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import io.vasilenko.moviedb.R
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 class TvShowsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-//    private var param1: String? = null
-//    private var param2: String? = null
-
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        arguments?.let {
-//            param1 = it.getString(ARG_PARAM1)
-//            param2 = it.getString(ARG_PARAM2)
-//        }
-//    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,15 +17,4 @@ class TvShowsFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.tv_shows_fragment, container, false)
     }
-
-//    companion object {
-//        @JvmStatic
-//        fun newInstance(param1: String, param2: String) =
-//            TvShowsFragment().apply {
-//                arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-//                    putString(ARG_PARAM2, param2)
-//                }
-//            }
-//    }
 }
