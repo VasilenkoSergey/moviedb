484d0f1e6b07aef2709dc345df018dce2d12051b
Sergey Vasilenko <sergey@vasilenko.io>
2021-04-01 03:35:30 +0300

    Implement tv shows screen

--- app/src/main/java/io/vasilenko/moviedb/data/MockRepository.kt
+++ app/src/main/java/io/vasilenko/moviedb/data/MockRepository.kt
@@ -32,6 +32,21 @@ object MockRepository {
         return moviesList
     }
 
+    fun getTvShows(): List<Movie> {
+
+        val moviesList = mutableListOf<Movie>()
+        for (x in 0..10) {
+            val movie = Movie(
+                title = "Spider-Man $x",
+                voteAverage = 10.0 - x,
+                imagePath = "https://m.media-amazon.com/images/M/MV5BYTk3MDljOWQtNGI2My00OTEzLTlhYjQtOTQ4ODM2MzUwY2IwXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_.jpg"
+            )
+            moviesList.add(movie)
+        }
+
+        return moviesList
+    }
+
     fun getWatchlistMovies(): List<Movie> {
 
         val moviesList = mutableListOf<Movie>()
--- app/src/main/java/io/vasilenko/moviedb/ui/tvshows/TvShowsFragment.kt
+++ app/src/main/java/io/vasilenko/moviedb/ui/tvshows/TvShowsFragment.kt
@@ -1,6 +1,32 @@
 package io.vasilenko.moviedb.ui.tvshows
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
 import io.vasilenko.moviedb.R
+import io.vasilenko.moviedb.data.MockRepository
+import kotlinx.android.synthetic.main.tv_shows_fragment.*
 
-class TvShowsFragment : Fragment(R.layout.tv_shows_fragment)
+class TvShowsFragment : Fragment(R.layout.tv_shows_fragment) {
+
+    private val adapter by lazy {
+        GroupAdapter<GroupieViewHolder>()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        val tvShows = MockRepository.getTvShows().map {
+            TvShowItem(it)
+        }.toList()
+        tvShowRecyclerView.adapter = adapter.apply {
+            addAll(tvShows)
+        }
+    }
+}
