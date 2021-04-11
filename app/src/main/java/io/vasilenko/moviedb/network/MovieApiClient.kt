8b61b2d008b397f454e858b9756e97e0f2c4d320
Sergey Vasilenko <sergey@vasilenko.io>
2021-04-11 13:15:43 +0300

    Add retrofit

--- app/src/main/java/io/vasilenko/moviedb/network/MovieApiClient.kt
+++ app/src/main/java/io/vasilenko/moviedb/network/MovieApiClient.kt
@@ -1,3 +1,45 @@
 package io.vasilenko.moviedb.network
 
-object MovieApiClient
+import io.vasilenko.moviedb.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object MovieApiClient {
+
+    private fun getRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl(BuildConfig.THE_MOVIE_DATABASE_API_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    private fun getOkHttp(): OkHttpClient {
+        val builder = OkHttpClient.Builder()
+
+        if (BuildConfig.DEBUG) {
+            builder.addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+        }
+
+        builder.addInterceptor { chain ->
+            chain.proceed(
+                chain.request()
+                    .newBuilder()
+                    .addHeader("Authorization", "Bearer ${BuildConfig.THE_MOVIE_DATABASE_API}")
+                    .url(
+                        chain.request().url
+                            .newBuilder()
+                            .addQueryParameter("language", "ru")
+                            .build()
+                    )
+                    .build()
+            )
+        }
+
+        return builder.build()
+    }
+}
